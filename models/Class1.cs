b8814011cdbf8b1196c6df53cab726eb9baf52f1
Dana Adolphus Moore <58271788+adolphus7777@users.noreply.github.com>
2022-06-30 18:33:31 -0400

    Created A Ticket Model Class

--- models/Class1.cs
+++ models/Class1.cs
@@ -1,5 +1,13 @@
 ﻿namespace models;
-public class Class1
+public class Ticket
 {
+    //This is an empty constructor
+    public Ticket() {}
 
+    public int ID{get; set;}
+    public int Author{get; set;}
+    public int Resolver{get; set;}
+    public string Description{get; set;}
+    public string Status{get; set;}
+    public decimal Amount{get; set;}
 }
